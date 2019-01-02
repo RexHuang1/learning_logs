a912945001682b74839abadcaca7022ec703a06b
Rex Huang <RexHuang_dev@163.com>
2019-01-02 18:08:55 +0800

    完成初始功能

---
+++ learning_logs/urls.py
@@ -0,0 +1,16 @@
+"""定义learning_logs的URL模式"""
+from django.conf.urls import url
+from . import views
+from django.urls import path,re_path
+
+app_name = 'learning_logs'
+urlpatterns = [
+    #主页
+    #url(r'^$',views.index,name='index'),
+    path('',views.index,name='index'),
+    path('topics/',views.topics,name='topics'),
+    re_path(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+    path('new_topic/',views.new_topic,name='new_topic'),
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$',views.new_entry,name='new_entry'),
+    re_path(r'^edit_entry/(?P<entry_id>\d+)/$',views.edit_entry,name='edit_entry'),
+]
